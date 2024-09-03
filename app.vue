79111d934c3b49093c73b17e2ddf142cea7a6cf3
Ariz <>
2024-09-03 10:50:53 +0800

    layouts

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component.
-    </AppAlert>
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
